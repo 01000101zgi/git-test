7b7cfbdc909192936ec14af43edc29f005326418
ezgi kızıl <ezgikizil@ezgi-MacBook-Pro.local>
2019-01-21 21:39:00 +0300

    Initial Commit

---
+++ git test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git test
+//
+//  Created by ezgi kızıl on 21.01.2019.
+//  Copyright © 2019 ezgi kızıl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
